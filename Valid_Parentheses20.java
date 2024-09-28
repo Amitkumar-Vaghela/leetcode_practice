765e855b1deaef239910133c4167e70e9ed28c83
Amit vaghela <138572130+Amitkumar-Vaghela@users.noreply.github.com>
2024-09-28 17:56:39 +0530

    Update Valid_Parentheses20.java

--- Valid_Parentheses20.java
+++ Valid_Parentheses20.java
@@ -29,3 +29,32 @@ class Solution {
         return st.isEmpty();
     }
 }
+
+
+/*
+
+Example 1:
+
+Input: s = "()"
+
+Output: true
+
+Example 2:
+
+Input: s = "()[]{}"
+
+Output: true
+
+Example 3:
+
+Input: s = "(]"
+
+Output: false
+
+Example 4:
+
+Input: s = "([])"
+
+Output: true
+
+*/
