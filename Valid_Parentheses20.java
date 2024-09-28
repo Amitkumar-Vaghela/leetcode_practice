2efe1eba70cad5408709b29bc5b821be394e86bb
Amit vaghela <138572130+Amitkumar-Vaghela@users.noreply.github.com>
2024-09-28 17:55:55 +0530

    Create  Valid_Parentheses20.java

---
+++ Valid_Parentheses20.java
@@ -0,0 +1,31 @@
+import java.util.Stack;
+
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            // Push the opening brackets into the stack
+            if (ch == '[' || ch == '(' || ch == '{') {
+                st.push(ch);
+            }
+            // Check for matching closing brackets and pop if valid
+            else if (ch == ']' && !st.isEmpty() && st.peek() == '[') {
+                st.pop();
+            } else if (ch == ')' && !st.isEmpty() && st.peek() == '(') {
+                st.pop();
+            } else if (ch == '}' && !st.isEmpty() && st.peek() == '{') {
+                st.pop();
+            }
+            // If none of the conditions match, return false
+            else {
+                return false;
+            }
+        }
+
+        // If the stack is empty, all brackets were valid, otherwise false
+        return st.isEmpty();
+    }
+}
