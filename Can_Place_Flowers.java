78a3cf7e62e02042f35342317003008bb101c0da
Amit vaghela <138572130+Amitkumar-Vaghela@users.noreply.github.com>
2024-10-13 20:13:53 +0530

    Create Can_Place_Flowers.java

---
+++ Can_Place_Flowers.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        int count = 0;
+        int length = flowerbed.length;
+        
+        for (int i = 0; i < length; i++) {
+            if (flowerbed[i] == 0) {
+                // Check the left and right plots
+                boolean emptyLeft = (i == 0 || flowerbed[i - 1] == 0);
+                boolean emptyRight = (i == length - 1 || flowerbed[i + 1] == 0);
+                
+                if (emptyLeft && emptyRight) {
+                    // Plant a flower here
+                    flowerbed[i] = 1;
+                    count++;
+                    
+                    // Skip the next plot
+                    if (count >= n) {
+                        return true;
+                    }
+                }
+            }
+        }
+        
+        return count >= n;
+    }
+}
