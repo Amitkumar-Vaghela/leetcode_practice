05e0465a7ba6ac715aa24558e8682d9ce28855b9
Amit vaghela <138572130+Amitkumar-Vaghela@users.noreply.github.com>
2025-01-14 19:51:17 +0530

    Create Longest_valid_Parentheses32.java

---
+++ Longest_valid_Parentheses32.java
@@ -0,0 +1,23 @@
+import java.util.Stack;
+
+public class Solution {
+    public int longestValidParentheses(String s) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(-1); // Base for the first valid substring
+        int maxLength = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                stack.push(i);
+            } else {
+                stack.pop(); // Pop for a match
+                if (stack.isEmpty()) {
+                    stack.push(i); // Push the current index as the base
+                } else {
+                    maxLength = Math.max(maxLength, i - stack.peek());
+                }
+            }
+        }
+        return maxLength;
+    }
+}
