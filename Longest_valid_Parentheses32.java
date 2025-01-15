e668cad9496e89f48ae502d422df388deaa3d83d
Amit vaghela <138572130+Amitkumar-Vaghela@users.noreply.github.com>
2025-01-15 21:22:32 +0530

    Update Longest_valid_Parentheses32.java

--- Longest_valid_Parentheses32.java
+++ Longest_valid_Parentheses32.java
@@ -21,3 +21,22 @@ public class Solution {
         return maxLength;
     }
 }
+
+/*
+
+Example 1:
+
+Input: s = "(()"
+Output: 2
+Explanation: The longest valid parentheses substring is "()".
+Example 2:
+
+Input: s = ")()())"
+Output: 4
+Explanation: The longest valid parentheses substring is "()()".
+Example 3:
+
+Input: s = ""
+Output: 0
+
+*/
