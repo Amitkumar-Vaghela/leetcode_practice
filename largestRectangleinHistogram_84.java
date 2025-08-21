459e763cea3cc6c14609da18b1e70571d26da45f
Amit vaghela <138572130+Amitkumar-Vaghela@users.noreply.github.com>
2025-08-21 22:24:51 +0530

    Create largestRectangleinHistogram_84.java

---
+++ largestRectangleinHistogram_84.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int largestRectangleArea(int[] heights) {
+        int n = heights.length;
+        int maxArea = 0;
+
+        for (int i = 0; i < n; i++) {
+            int minHeight = Integer.MAX_VALUE;
+            for (int j = i; j < n; j++) {
+                minHeight = Math.min(minHeight, heights[j]);
+                maxArea = Math.max(maxArea, minHeight * (j - i + 1));
+            }
+        }
+
+        return maxArea;
+    }
+}
+
+
+// Example 1:
+// Input: heights = [2,1,5,6,2,3]
+// Output: 10
+// Explanation: The above is a histogram where width of each bar is 1.
+// The largest rectangle is shown in the red area, which has an area = 10 units.
